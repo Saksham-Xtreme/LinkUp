70b658d60b4cf50cead987893460bedbcafe4f2c
Saksham-Xtreme <shivbhau2108@gmail.com>
2026-03-10 18:44:38 +0530

    added footer

--- linkup-frontend/src/pages/home.jsx
+++ linkup-frontend/src/pages/home.jsx
@@ -5,7 +5,7 @@ import VideoCallIcon from "@mui/icons-material/VideoCall";
 import KeyboardIcon from "@mui/icons-material/Keyboard";
 import RestoreIcon from '@mui/icons-material/Restore';
 import { useAuth } from "../contexts/AuthContext"; 
-import Footer from './components/Footer';
+import Footer from '../components/Footer';
 import "../styles/home.css"; // Ensure this path matches where your CSS file is saved
 
 export default function Home() {
